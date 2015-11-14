28bc8be432e3db584a406813a62b5135a2294e8c
crewstyle <ach@takeatea.com>
2015-11-14 13:01:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Flexy [![Build Status](https://travis-ci.org/crewstyle/flexy.svg)](https://travis-ci.org/crewstyle/flexy)
+
+_an iframe responsive jQuery plugin_
+
+
+## Summary
+
++ [1) What is this all about?](#1-what-is-this-all-about)
++ [2) Install](#2-install)
++ [3) How to use it](#3-how-to-use-it)
++ [4) That's all folkes!](#4-thats-all-folkes)
++ [5) Authors and Copyright](#5-authors-and-copyright)
+
+
+## 1) What is this all about?
+
+[Flexy](https://github.com/crewstyle/flexy) is a very light jQuery iframe responsive plugin.  
+
+
+## 2) Install
+
+If you use [Bower.io](http://bower.io), you can download *Flexy* and use it with:
+
+````
+bower install --save yohoho.flexy
+````
+
+
+## 3) How to use it
+
+It's pretty simple to use the *Flexy* jQuery package. It's a two-steps install.  
+In your HTML:
+
+````html
+<iframe width="560" height="315" src="https://www.youtube.com/embed/x3kfyZJhC3U?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+````
+
+In your Javascript:
+````javascript
+$(document).ready({
+    $('iframe').flexy({
+        addcss: true,
+        classname: 'flexy-wrapper',
+        widescreen: true
+    });
+});
+````
+
+
+## 4) That's all folkes!
+
+Now let's have fun :)
+
+
+## 5) Authors and Copyright
+
+**Achraf Chouk**
+
++ http://fr.linkedin.com/in/achrafchouk/
++ http://twitter.com/crewstyle
++ http://github.com/crewstyle
+
+Please, read [LICENSE](https://github.com/crewstyle/flexy/blob/master/LICENSE "LICENSE") for more details.
+
+Copyright 20xx [Achraf Chouk](http://github.com/crewstyle "Achraf Chouk").  
+Brewed by Achraf Chouk, served by [Take a tea](http://www.takeatea.com "Take a tea") ;)
---
+++ flexy.js
@@ -0,0 +1,123 @@
+/*! *//*!
+ * flexy.js v1.0.0 - "Sogeking no shima deeeeeee - One Piece"
+ * ~~~~~~~~~~~~~~~~~~
+ *
+ * Example of use HTML:
+ * <iframe width="560" height="315" src="https://www.youtube.com/embed/x3kfyZJhC3U?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+ *
+ * Example of use JS:
+ * $('iframe').flexy({
+ *     addcss: true,
+ *     classname: 'flexy-wrapper',
+ *     widescreen: true,
+ * });
+ *
+ * ~~~~~~~~~~~~~~~~~~
+ * Copyright 2015 Achraf Chouk <achrafchouk@gmail.com>
+ */
+
+(function ($){
+    "use strict";
+    var Flexy = function ($el, options){
+        //vars
+        var _flexy = this;
+        _flexy.$el = $el;
+        _flexy.options = $.extend({}, options);
+
+        //initialize
+        _flexy.initialize();
+    };
+
+    Flexy.prototype.options = {};
+    Flexy.prototype.$el = null;
+    Flexy.prototype.$wrap = null;
+
+    Flexy.prototype.initialize = function (){
+        var _flexy = this;
+
+        //create wrapper
+        _flexy.$el.wrap('<div class="' + _flexy.options.classname + '" />');
+        _flexy.$wrap = _flexy.$el.closest('.' + _flexy.options.classname);
+
+        //add CSS to wrapper
+        if (_flexy.options.addcss) {
+            _flexy.addCss();
+        }
+    };
+
+    Flexy.prototype.addCss = function (){
+        var _flexy = this,
+            _src = _flexy.$el.attr('src');
+
+        //add CSS to element
+        _flexy.$el.css({
+            boxSizing: 'border-box',
+            height: '100%',
+            left: 0,
+            position: 'absolute',
+            top: 0,
+            width: '100%',
+        });
+
+        //add CSS to wrapper
+        _flexy.$wrap.css({
+            boxSizing: 'border-box',
+            height: 0,
+            overflow: 'hidden',
+            paddingBottom: '67.5%',
+            paddingTop: _flexy.toRem(25) + 'rem',
+            position: 'relative',
+        });
+
+        //wide screens
+        if (_flexy.options.widescreen) {
+            _flexy.$wrap.css('padding-bottom', '56.34%');
+        }
+
+        //vimeo
+        if (_src.match(/player\.vimeo\.com/)) {
+            _flexy.$wrap.css('padding-top', 0);
+        }
+    };
+
+    Flexy.prototype.toRem = function (length){
+        var _rem = parseFloat($('html').css('font-size'));
+        return (parseInt(length) / _rem);
+    };
+
+    var methods = {
+        init: function (options){
+            if (!this.length) {
+                return false;
+            }
+
+            var settings = {
+                addcss: true,
+                classname: 'flexy-wrapper',
+                widescreen: true
+            };
+
+            return this.each(function (){
+                if (options) {
+                    $.extend(settings, options);
+                }
+
+                new Flexy($(this), settings);
+            });
+        },
+        update: function (){},
+        destroy: function (){}
+    };
+
+    $.fn.flexy = function (method){
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        }
+        else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        }
+        else {
+            $.error('Method ' + method + ' does not exist on jQuery.Flexy');
+        }
+    };
+})(window.jQuery);
